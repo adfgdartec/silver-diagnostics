5b37ae20ff90090d67fd7e0cf9daa7a289cc832d
Adit Rajaram <rajitadarth422@gmail.com>
2026-08-15 12:14:24 -0500

    Release 0.2.0

--- src/silver_diagnostics/__init__.py
+++ src/silver_diagnostics/__init__.py
@@ -3,3 +3,5 @@ from .metrics import diagnose_metrics
 from .models import Diagnostic, DiagnosticReport
 
 __all__ = ["Diagnostic", "DiagnosticReport", "diagnose_dataset", "diagnose_metrics"]
+
+__version__ = "0.2.0"
