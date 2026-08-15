d5cc26ee31f46353728353c31f7821ab567b07f2
Adit Rajaram <rajitadarth422@gmail.com>
2026-08-15 14:00:33 -0500

    Release 0.3.0 with supervised learning examples

--- src/silver_diagnostics/__init__.py
+++ src/silver_diagnostics/__init__.py
@@ -8,10 +8,12 @@ from .classification import (
 from .metrics import diagnose_metric_history, diagnose_metrics
 from .models import Diagnostic, DiagnosticReport
 from .model import diagnose_model_spec
+from .supervised import assess_supervised_system, AlgorithmFamily, DatasetRequirement, LearningPrinciple, SupervisedAssessment
 
 __all__ = ["Diagnostic", "DiagnosticReport", "diagnose_dataset", "diagnose_metrics", "diagnose_metric_history",
            "ConfusionMatrixReport", "HierarchicalConfusionReport", "analyze_confusion_matrix",
            "analyze_hierarchical_confusion"]
 __all__.append("diagnose_model_spec")
+__all__ += ["LearningPrinciple", "DatasetRequirement", "AlgorithmFamily", "SupervisedAssessment", "assess_supervised_system"]
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
